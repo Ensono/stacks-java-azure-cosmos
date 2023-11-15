05ffbaa56b52f9c1e2051ca7b937a6af12798057
Giuseppe Falco-Scampatelli <giuseppefalco-scampatelli@ens-q4dh0j0mw6.cust.communityfibre.co.uk>
2023-11-15 17:26:23 +0000

    updated versions, properties, docker images

--- java/src/main/java/com/amido/stacks/workloads/menu/repository/MenuRepository.java
+++ java/src/main/java/com/amido/stacks/workloads/menu/repository/MenuRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CosmosMenuRepository extends StacksCosmosRepository<Menu> {
+public interface MenuRepository extends StacksCosmosRepository<Menu> {
 
   @Override
   Menu save(Menu menu);
